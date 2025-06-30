de2eeb289fc02d97c505c66803bba63efe59eabc
nct1673 <nct5308@gmail.com>
2025-06-30 13:35:09 +0800

    basic dashboard

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
 import json
+from config import server_config
 
 app = Flask(__name__)
 
 @app.route('/')
 def dashboard():
-    with open("/home/chen/Desktop/weather_web/data/pred_out.json") as f:
+    filename = f'/home/{server_config}/weather_web/data/pred_out.json'
+    with open(filename) as f:
         data = json.load(f)
-    # return render_template("/home/chen/Desktop/weather_web/templates/dashboard.html", data=data)
     return render_template("dashboard.html", data=data)
 
+@app.route('/details')
+def details():
+    filename = f'/home/{server_config}/weather_web/data/pred_out.json'
+    with open(filename) as f:
+        data = json.load(f)
+    return render_template("details.html", data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
